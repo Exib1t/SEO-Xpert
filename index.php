bd32ecb6d78f25a4eb6249f52282c43810b2206c
Exib1t <dmaznyak2604@gmail.com>
2022-07-06 22:35:25 +0300

    repo create

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$pageName = 'Home';
+$siteName = 'SEO Xpert';
+$uri      = 'home';
+require( 'inc/header.php' );
+?>
+
+<main class="l-contentText">
+  <section class="l-firstScreen" style="background-image: url('assets/img/userfiles/firstScreen-BG.png')">
+    <div class="b-firstScreen">
+      <h1 class="b-firstScreen__title">All-inclusive SEO software made simple</h1>
+      <div class="b-firstScreen__text b-text">
+        <p>If the path is beautiful, let us not k where it leads. my religion is very simple.</p>
+      </div>
+      <a href="#" class="b-firstScreen__button">Sign Up</a>
+    </div>
+  </section>
+</main>
+
+<?php require( 'inc/footer.php' ); ?>
